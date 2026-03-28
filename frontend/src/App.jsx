e064b5d00b0234b4e8d521a538e7dac339e5218e
SHREYA G AMIN <shreyagamin@gmail.com>
2026-03-29 01:17:50 +0530

    app.jsx fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Welcome from "./pages/Welcome";
 
@@ -25,8 +14,3 @@ function App() {
 }
 
 export default App;
-    </>
-  )
-}
-
-export default App
